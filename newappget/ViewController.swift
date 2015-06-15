689249101ef1114e74a0d6da94e2ca6cb18b9092
taiki.ogasawara <taiki.ogasawara@gmodecorp.com>
2015-06-15 09:21:11 +0900

    プロジェクト作成

---
+++ newappget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newappget
+//
+//  Created by taiki.ogasawara on 2015/06/15.
+//  Copyright (c) 2015年 ogataiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
